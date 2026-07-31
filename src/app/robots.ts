d8112b774c057bbd1ab23899514062fec0aaed18
Arkhemis <80684272+Arkhemis@users.noreply.github.com>
2026-07-31 20:04:06 +0200

    feat: ajouter des règles pour bloquer l'accès à plusieurs bots dans robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,18 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       { userAgent: "ia_archiver", disallow: "/" },
       { userAgent: "archive.org_bot", disallow: "/" },
+      { userAgent: "GPTBot", disallow: "/" },
+      { userAgent: "ChatGPT-User", disallow: "/" },
+      { userAgent: "OAI-SearchBot", disallow: "/" },
+      { userAgent: "ClaudeBot", disallow: "/" },
+      { userAgent: "Claude-Web", disallow: "/" },
+      { userAgent: "anthropic-ai", disallow: "/" },
+      { userAgent: "Google-Extended", disallow: "/" },
+      { userAgent: "CCBot", disallow: "/" },
+      { userAgent: "Bytespider", disallow: "/" },
+      { userAgent: "PerplexityBot", disallow: "/" },
+      { userAgent: "Meta-ExternalAgent", disallow: "/" },
+      { userAgent: "Applebot-Extended", disallow: "/" },
     ],
   };
 }
